b9a2e60703d9f15fbac18120e4e627f49b81373a
Ruslan Kardanov <ruslan.kardanov@myscenario.de>
2016-07-26 19:21:05 +0200

    Rolling back

--- src/js/utils/version.js
+++ src/js/utils/version.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('myApp.version', [ 'myApp.version.interpolate-filter', 'myApp.version.version-directive' ]).value('version', '0.38');
+angular.module('myApp.version', [ 'myApp.version.interpolate-filter', 'myApp.version.version-directive' ]).value('version', '0.39');

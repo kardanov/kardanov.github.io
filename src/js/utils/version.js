030ba2bae918f1b7f775791860728dc101150a67
Ruslan Kardanov <ruslan.kardanov@myscenario.de>
2016-07-27 15:47:02 +0200

    Testing GitHub desktop
    Version of the app increased

--- src/js/utils/version.js
+++ src/js/utils/version.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('myApp.version', [ 'myApp.version.interpolate-filter', 'myApp.version.version-directive' ]).value('version', '0.39');
+angular.module('myApp.version', [ 'myApp.version.interpolate-filter', 'myApp.version.version-directive' ]).value('version', '0.40');

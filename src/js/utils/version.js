eef5a678e865f2d7eb08407f05842f28ac6811ed
Ruslan Kardanov <ruslan.kardanov@myscenario.de>
2016-08-12 15:28:24 +0200

    Minor updates and bug fixing.

--- src/js/utils/version.js
+++ src/js/utils/version.js
@@ -1,3 +1,3 @@
 'use strict';
 
-angular.module('myApp.version', [ 'myApp.version.interpolate-filter', 'myApp.version.version-directive' ]).value('version', '0.52');
+angular.module('myApp.version', [ 'myApp.version.interpolate-filter', 'myApp.version.version-directive' ]).value('version', '0.53');

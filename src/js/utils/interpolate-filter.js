2a1f5e2f40efb035391b3771a504df9b54ab3361
Ruslan Kardanov <ruslan.kardanov@myscenario.de>
2017-01-03 15:20:49 +0100

    Some new places added to the travel map. Updates for the year 2017.

--- src/js/utils/interpolate-filter.js
+++ src/js/utils/interpolate-filter.js
@@ -1,5 +1,4 @@
 'use strict';
-
 angular.module('myApp.version.interpolate-filter', []).filter('interpolate', ['version', function(version) {
     return function(text) {
         return String(text).replace(/\%VERSION\%/mg, version);

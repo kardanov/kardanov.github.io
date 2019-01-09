74a182041e0949995f79d4838386a9417ad2ea9e
Ruslan Kardanov <ruslankardanov@ruslans-mini.localdomain>
2019-01-09 21:21:29 +0100

    New recent places added to the travel map

--- src/js/controllers/travel-controller.js
+++ src/js/controllers/travel-controller.js
@@ -11,7 +11,7 @@ travelController = function($scope, $http, $mdSidenav, $timeout, dataFactory) {
     $scope.tech.flag = '';
     $scope.tech.country = '';
     $scope.tech.city = '';
-    $scope.tech.year = 2017;
+    $scope.tech.year = 2019;
     $scope.tech.all = [];
 
     // Setting up custom map marker icon.
